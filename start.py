dd1aa9f180ca7b2063e3c51132d7ef9414b37bd3
munix24 <munix24@gmail.com>
2026-01-07 14:37:38 -0500

    bug in start.py

--- start.py
+++ start.py
@@ -1,51 +1,57 @@
 ## job has to be repeatable and idempotent
+import os
+import logging
+from pathlib import Path
+
+# Configure logging from environment variables before importing application modules
+# - Else if LOG_LEVEL is set (e.g. INFO, WARNING), use that
+_log_level_env = os.getenv('LOG_LEVEL')
+if _log_level_env:
+    _level = getattr(logging, _log_level_env.upper(), logging.INFO)
+else:
+    _level = logging.INFO
+
+logging.basicConfig(level=_level, format='%(message)s')
+logging.getLogger('pymongo').setLevel(logging.INFO)             # don't flood log with pymongo debug info
+
+# Now import application modules (they will get configured logging)
 import API_league_v4
 import API_matches
 import API_match
 import DB_client
 
-def is_matchID_after_threshold(matchID, region_prefix = "NA1_", threshold = 5_000_000_000) -> bool:
+logger = logging.getLogger(__name__)
+# Announce the configured root logging level so startup logs show it
+logger.info("Logging configured; root level=%s", logging.getLevelName(_level))
+
+# 5420859667 is latest gameID of 15.23
+def is_matchID_after_threshold(matchID, region_prefix = "NA1_", threshold = 5_421_000_000) -> bool:
     try:
         return int(matchID[len(region_prefix):]) > threshold
     except ValueError:
         return False
-
-def lookup_and_process_matches_for_oldest_ranked_puuids(DEBUG=False):
+          
+# filter bad games, otherwise will use up resources querying later
+def should_process_match(match_json, queue_id=420, min_duration=500) -> bool:
     try:
-        while True:
-            df_puuids = DB_client.db.select_oldest_ranked_puuids_df()
-            for puuid in df_puuids['puuid']:
-                if DEBUG:
-                    print(puuid)
-                matchIDs_list = API_matches.get_matches_API_json_by_puuid(puuid)                      # even if null continue to update puuid
-                if DEBUG:
-                    print('total matchIDs for puuid:', len(matchIDs_list))
-
-                matchIDs_list = [m for m in (matchIDs_list or []) if is_matchID_after_threshold(m)]
-                if DEBUG:
-                    print('total matchIDs above threshold:', len(matchIDs_list))
-
-                matchIDs_list = DB_client.db.select_matches_in_list_not_in_table(matchIDs_list)       # even if null continue to update puuid
-                if DEBUG:
-                    print('new matchIDs to process:', len(matchIDs_list))
-
-                for matchID in matchIDs_list:
-                    if DEBUG:
-                        print('processing matchID:', matchID)
-
-                    match_json = API_match.get_match_API_json_by_matchID(matchID)
-
-                    if match_json['info']['endOfGameResult'] == 'GameComplete':   # skip incomplete games
-                        try:
-                            txn = DB_client.db.begin_transaction()
-
-                            # Only insert participants in SQL. MongoDB participants are part of match document
-                         if not info.get('gameVersion', '').startswith(('15.24', '16.1')):
+        info = match_json.get('info', {})
+        if info.get('endOfGameResult') != 'GameComplete':       # skip ongoing games?
+            logger.info('endOfGameResult) != GameComplete')
+            return False
+        if info.get('queueId') != queue_id:                     # only ranked solo
+            logger.info('queueId != %s', queue_id)
+            return False
+        if info.get('gameDuration', 0) <= min_duration:         # not earlySurrender
+            logger.info('gameDuration <= %s', min_duration)
+            return False
+        if not info.get('gameVersion', '').startswith(('15.24', '16.1')):
             logger.info('gameVersion does not start with 15.24 or 16.1: %s', info.get('gameVersion', ''))
             return False
-           if DB_client.db.__class__.__name__ == 'SqlDBClient':
+        return True
+    except Exception:
+        return False
 
-def lookup_and_process_matches_for_orecent_matches()n
+def lookup_and_process_matches_for_recent_matches():
     try:
         while True:
             df_matches = DB_client.db.select_most_recent_matches()
@@ -108,21 +114,40 @@ def process_puuid(puuid, get_league_v4_API_json):
             finally:
                 DB_client.db.close_transaction(session)
 
-def lookup_matches_and_leagues_v4_for_oldest_ranked_puuids():ts_no_commit(matchID, match_json['info']['parcursor'], txn)
-
-                         puuids(tic            for doc in cursor:
+def lookup_matches_and_leagues_v4_for_oldest_ranked_puuids(start: int = 0, count: int = 100):
+    try:
+        while True:
+            cursor = DB_client.db.select_oldest_ranked_puuids(start=start, count=count)
+            for doc in cursor:
                 puuid = doc['puuid']
                 process_puuid(puuid, get_league_v4_API_json = True)
-nk                leagues_v4_json = API_league_v4.get_league_v4_API_json_by_puuid(puuid)y:                DB_client.db.merge_league_v4(puuid, leagues_v4_json)puuid(puuid, get_league_v4_API_json)
 
-                if get_league_v4_API_json:
-                    leagues_v4_json = API_league_v4.get_league_v4_API_json_by_puuid(puuid)
-         B_client.db.merge_league_v4(puuid, leagues_v4_json)
-                el
+                leagues_v4_json = API_league_v4.get_league_v4_API_json_by_puuid(puuid)
+                DB_client.db.merge_league_v4(puuid, leagues_v4_json)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt used. Shutting down...")
+    except Exception as e:
+        print("Error occured: ", e)
+        raise
+
+def lookup_matches_for_recent_match_participants():
+    try:
+        while True:
             cursor = DB_client.db.select_most_recent_matches()
             for doc in cursor:
                 matchID = doc['matchID']
                 logger.info('matchID: %s', matchID)
                 for participant in doc['participants']:
-                    puuid = participant['puuid']t(                    pt used. Shutting dowget_league_v4_API_json = Falseept                lookup_matches_update_MatchesUtc_matchamatchIDpuuids
-    lookup_matches_for_recent_match_participants())# # # # 
+                    puuid = participant['puuid']
+                    process_puuid(puuid, get_league_v4_API_json = False)
+
+                DB_client.db.update_MatchesUtc_match(matchID)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt used. Shutting down...")
+    except Exception as e:
+        print("Error occured: ", e)
+        raise
+
+if __name__ == "__main__":
+    # lookup_matches_and_leagues_v4_for_oldest_ranked_puuids()
+    lookup_matches_for_recent_match_participants()
